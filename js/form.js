7446e5d6e6cbdaa8e359a7228e7cad94c8e2e891
Vanesflat <ptptcptptc@mail.ru>
2022-11-17 11:42:57 +0700

    Realized functional to change scale

--- js/form.js
+++ js/form.js
@@ -1,24 +1,44 @@
 import { commentField, hashtagsField, pristine, uploadForm } from './validate.js';
 
+const SCALE_SETTINGS = {
+  MIN_VALUE: 25,
+  MAX_VALUE: 100,
+  STEP: 25,
+  DEFAULT_VALUE: 100
+};
 const uploadOverlay = uploadForm.querySelector('.img-upload__overlay');
-const uploadField = document.querySelector('#upload-file');
+const uploadField = uploadForm.querySelector('#upload-file');
 const closeButton = uploadForm.querySelector('#upload-cancel');
+const scaleField = uploadForm.querySelector('.scale__control--value');
+const scaleUpButton = uploadForm.querySelector('.scale__control--bigger');
+const scaleDownButton = uploadForm.querySelector('.scale__control--smaller');
+const previewPicture = uploadForm.querySelector('.img-upload__preview img');
 
-const closeUploadFormHandler = () => {
+const setDefaultScaleValue = () => {
+  scaleField.value = `${SCALE_SETTINGS.DEFAULT_VALUE}%`;
+  previewPicture.style.scale = scaleField.value;
+};
+
+const resetForm = () => {
   uploadField.value = '';
   hashtagsField.value = '';
   commentField.value = '';
   pristine.reset();
+};
+
+const closeUploadFormHandler = () => {
   uploadOverlay.classList.add('hidden');
-  document.body.classList.remove('.modal-open');
+  resetForm();
+  document.body.classList.remove('modal-open');
   document.removeEventListener('keydown', escapeKeydownHandler);
   closeButton.removeEventListener('click', closeUploadFormHandler);
   uploadOverlay.removeEventListener('click', uploadOverlayClickHandler);
 };
 
 const showUploadForm = () => {
+  setDefaultScaleValue();
   uploadOverlay.classList.remove('hidden');
-  document.body.classList.add('.modal-open');
+  document.body.classList.add('modal-open');
   document.addEventListener('keydown', escapeKeydownHandler);
   closeButton.addEventListener('click', closeUploadFormHandler);
   uploadOverlay.addEventListener('click', uploadOverlayClickHandler);
@@ -39,9 +59,26 @@ function escapeKeydownHandler(evt) {
   }
 }
 
+scaleUpButton.addEventListener('click', () => {
+  const scaleValueNumber = parseInt(scaleField.value, 10);
+  if (scaleValueNumber !== SCALE_SETTINGS.MAX_VALUE) {
+    scaleField.value = `${scaleValueNumber + SCALE_SETTINGS.STEP}%`;
+    previewPicture.style.scale = scaleField.value;
+  }
+});
+
+scaleDownButton.addEventListener('click', () => {
+  const scaleValueNumber = parseInt(scaleField.value, 10);
+  if (scaleValueNumber !== SCALE_SETTINGS.MIN_VALUE) {
+    scaleField.value = `${scaleValueNumber - SCALE_SETTINGS.STEP}%`;
+    previewPicture.style.scale = scaleField.value;
+  }
+});
+
 uploadField.addEventListener('change', showUploadForm);
 
 uploadForm.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  pristine.validate();
+  if (!pristine.validate()) {
+    evt.preventDefault();
+  }
 });
