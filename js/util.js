e16ab32bf8ebc5c06ac1f7e85c40e5410bc1d519
Vanesflat <ptptcptptc@mail.ru>
2022-11-19 22:18:11 +0700

    Added new functions for debounce and get random element array

--- js/util.js
+++ js/util.js
@@ -11,4 +11,25 @@ const checkStringLength = (string, maxLength) => string.length <= maxLength;
 
 const getRandomArrayElement = (arr) => arr[getRandomInteger(0, arr.length - 1)];
 
-export { getRandomInteger, checkStringLength, getRandomArrayElement };
+const getUniqueRandomElementsArray = (arr, length) => {
+  const someValues = [];
+  while (someValues.length < length) {
+    const currentElement = getRandomArrayElement(arr);
+    if (!someValues.includes(currentElement)) {
+      someValues.push(currentElement);
+    }
+  }
+  return someValues;
+};
+
+const debounce = (callback, timeoutDelay = 500) => {
+
+  let timeoutId;
+
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+export { getRandomInteger, checkStringLength, getUniqueRandomElementsArray, debounce };
